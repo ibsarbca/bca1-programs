66cfb736b3bf2659ffcfd72639300369335a93c6
ibsarbca <--global>
2016-08-12 19:47:33 +0530

    Did not actually add the new file because I am an idiot

---
+++ VB.NET/ExceptionsInExceptions/ExceptionsInExceptions/Module1.vb
@@ -0,0 +1,76 @@
+﻿Imports System.IO
+
+Module Module1
+
+    Sub Main()
+        ExceptionWithExceptions.Execute()
+        Console.Read()
+    End Sub
+
+End Module
+
+Public Class ExceptionWithExceptions
+
+    ''' <summary>
+    ''' Function exists to simple execute code without messing up other code in the project
+    ''' It's like a main function if you already have a Main() function and don't want to mess with it
+    ''' </summary>
+    ''' <returns>Nothing</returns>
+    ''' <remarks></remarks>
+    Public Shared Function Execute()
+        NestedExceptionWithThrow()
+    End Function
+
+    ''' <summary>
+    ''' This function does the same thing as NestedExceptionsUseCase(), only we do it manually by throwing the exception
+    ''' </summary>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Public Shared Function NestedExceptionWithThrow()
+        Try
+            ' Some code that throws NRE
+            Throw New NullReferenceException("CUSTOM EXCEPTION MESSAGE")
+        Catch ex As NullReferenceException
+            Console.WriteLine("NRE caught. Message: " + ex.Message)
+            Try
+                Throw ex
+            Catch ex
+                ' Different catch block. Here, ex will be bound to a new exception
+                Console.WriteLine("Ex (our NRE) is rethrown. Message: " + ex.Message)
+            End Try
+        End Try
+    End Function
+
+    ''' <summary>
+    ''' Here we'll try to access a file locally. If the file doesn't exist, or is not available, 
+    ''' we will check a remote backup of the file.
+    ''' If that doesn't exist either, then we say that the file is simply unavailable.
+    ''' </summary>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Public Shared Function NestedExceptionUseCase()
+        Dim fs
+        Try
+            ' Try to access the file.
+            fs = New FileStream("myfile.txt", System.IO.FileMode.Open)
+        Catch myIOE As Exception
+            ' print the value a unique member associated with that instance
+            Console.WriteLine(myIOE.GetHashCode())
+
+            ' If there's an exception, it means that the file is not available locally.
+            ' We try to access it through some imaginary network share
+
+            Try
+                ' Assuming some mapped drive Z:\
+                fs = New FileStream("Z:\myfile.txt", System.IO.FileMode.Open)
+            Catch myIOE ' This is a different object because it's a new exception
+                ' This means that the file is not available remotely either.
+                ' So we let the user deal with it.
+                Console.WriteLine(myIOE.GetHashCode)
+                ' The HashCode is different because it is bound to a new Exception
+                Console.WriteLine("All sources checked. The file is myfile.txt is missing")
+            End Try
+        End Try
+    End Function
+End Class
+
---
+++ VB.NET/ExceptionsInExceptions/ExceptionsInExceptions/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ExceptionsInExceptions")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("ExceptionsInExceptions")> 
+<Assembly: AssemblyCopyright("Copyright ©  2016")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d9f37da7-c40e-4407-bba3-7bc0ec20a3d2")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
