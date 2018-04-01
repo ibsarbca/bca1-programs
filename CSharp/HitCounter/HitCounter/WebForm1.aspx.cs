3258b0e36334b1bd2c81a6e3bd2b879dcf8ecd05
ibsarbca <--global>
2018-04-01 22:18:26 +0530

    Removed Calculator program that did not work.
    Also added AddRotator and HitCounter. However that code was written a long time before this commit and apparently never committed so it may need to be reviewed.

---
+++ CSharp/HitCounter/HitCounter/WebForm1.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HitCounter
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int hitcounter = Convert.ToInt32(Application["hitcounter"]);
+            hitcounter++;
+            Application["hitcounter"] = hitcounter;
+        }
+    }
+}
---
+++ CSharp/may2017/ShoppingCart/ShoppingCart/Global.asax.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace ShoppingCart
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            HttpCookie cookie = new HttpCookie("cart");
+            cookie.Values.Add("Tables", "0");
+            cookie.Values.Add("Chair", "0");
+            cookie.Values.Add("Bottles", "0");
+
+            Response.Cookies.Add(cookie);
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
